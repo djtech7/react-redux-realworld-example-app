32e5caffc0cc4bb77458b4e06abe41bef2f94cbe
Don Davis <djtech7@yahoo.com>
2020-06-07 17:29:37 -0400

    testing

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = ({ appName, token }) => {
         <h1 className="logo-font">
           {appName.toLowerCase()}
         </h1>
-        <p>A Toptal's place to share your knowledge.</p>
+        <p>A Toptal's best place to share your knowledge.</p>
       </div>
     </div>
   );
